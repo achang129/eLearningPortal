33ee972c12f10b9ef555bac56cba6291ee9c3f8d
Liam Knight <liamjknight@gmail.com>
2020-12-07 18:54:42 -0500

    Message DAO Donezo

--- java/src/main/java/com/techelevator/dao/MessageSqlDAO.java
+++ java/src/main/java/com/techelevator/dao/MessageSqlDAO.java
@@ -1,29 +1,94 @@
 package com.techelevator.dao;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.support.rowset.SqlRowSet;
+
+import com.techelevator.model.Message;
+
 public class MessageSqlDAO implements MessageDAO {
+	
+	private JdbcTemplate jdbc = new JdbcTemplate();
+	
+	
 
 	@Override
-	public int getMessageID() {
-		// TODO Auto-generated method stub
-		return 0;
+	public boolean send(int user, String content) {
+		String sql = "INSERT INTO message (user_id, read, content) " +
+					 "VALUES (?, 0, ?)";
+		
+		int worked = jdbc.update(sql, user, content);
+		
+		if(worked > 0) {
+			return true;
+		}else {
+			return false;
+		}
 	}
 
 	@Override
-	public int getRecipientUserID() {
-		// TODO Auto-generated method stub
-		return 0;
+	public List<Message> getList(int user) {
+		List<Message> results = new ArrayList<Message>();
+		String sql = "SELECT * FROM message WHERE user = ?";
+		
+		SqlRowSet rowSet = this.jdbc.queryForRowSet(sql, user);
+		
+		while(rowSet.next()) {
+			results.add(mapToMessage(rowSet));
+		}
+		
+		
+		return results;
 	}
 
 	@Override
-	public boolean isRead() {
-		// TODO Auto-generated method stub
-		return false;
+	public boolean isRead(int id) {
+		boolean result = false;
+		String sql = "SELECT read FROM message WHERE id = ?";
+		
+		SqlRowSet rowSet = jdbc.queryForRowSet(sql, id);
+		result = rowSet.getBoolean("id");
+		
+		return result;
 	}
 
 	@Override
-	public String getContent() {
-		// TODO Auto-generated method stub
-		return null;
+	public boolean markRead(int id) {
+		String sql = "UPDATE message (read = 1) WHERE id = ?";
+		
+		int worked = jdbc.update(sql, id);
+		
+		if(worked > 0) {
+			return true;
+		}else {
+			return false;
+		}
+	}
+
+	@Override
+	public boolean delete(int id) {
+		String sql = "DELETE * FROM message WHERE id = ?";
+		
+		int worked = jdbc.update(sql, id);
+		
+		if(worked > 0) {
+			return true;
+		}else {
+			return false;
+		}
+	}
+	
+	private Message mapToMessage(SqlRowSet rowSet) {
+		Message result = new Message();
+		
+		result.setMessageID(rowSet.getInt("id"));
+		result.setUserID(rowSet.getInt("user_id"));
+		result.setRead(rowSet.getBoolean("read"));
+		result.setContent(rowSet.getString("content"));
+		
+		return result;
 	}
 
 }
--- java/src/main/java/com/techelevator/model/Message.java
+++ java/src/main/java/com/techelevator/model/Message.java
@@ -1,5 +1,46 @@
 package com.techelevator.model;
 
 public class Message {
-
+	//properties
+	int messageID;
+	int userID;
+	boolean isRead;
+	String content;
+	
+	//getters/setters
+	public int getMessageID() {
+		return messageID;
+	}
+	public void setMessageID(int messageID) {
+		this.messageID = messageID;
+	}
+	public int getUserID() {
+		return userID;
+	}
+	public void setUserID(int userID) {
+		this.userID = userID;
+	}
+	public boolean isRead() {
+		return isRead;
+	}
+	public void setRead(boolean isRead) {
+		this.isRead = isRead;
+	}
+	public String getContent() {
+		return content;
+	}
+	public void setContent(String content) {
+		this.content = content;
+	}
+	
+	//constructors
+	public Message() { }
+	
+	public Message(int messageID, int userID, boolean isRead, String content) {
+		this.messageID = messageID;
+		this.userID = userID;
+		this.isRead = isRead;
+		this.content = content;
+	}
+	
 }
