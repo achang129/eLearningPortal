857c8f3d2f89a6d0eb84116fc3e70db59c878c83
Andy Chang <achang129@yahoo.com>
2020-12-09 18:15:29 -0500

    Added a very simplistic progress bar with fixed values

--- vue/src/components/ProgressBar.vue
+++ vue/src/components/ProgressBar.vue
@@ -1,11 +1,19 @@
 <template>
     <div class="progress">
-        <div class="bar" :style="'height:30px;background:blue;width:' + progress + '%'"></div>
+        <div class="bar" :style="'height:30px;background:blue;width:' + progress() + '%'"></div>
+        <div>{{ progress() }}% Completed</div>
     </div>
 </template>
 
 <script>
 export default {
+    name: 'progress-bar',
+    data() {
+        return {
+            completed: 5,
+            total: 10
+        }
+    },
     methods: {
         progress(){
             return this.completed / this.total * 100
