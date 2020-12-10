ef9be282cf7bfddf41210eb0aff4ff920a53cc65
Andy Chang <achang129@yahoo.com>
2020-12-10 12:36:44 -0500

    Minor Edits to Course and Homework

--- vue/src/views/AllHomework.vue
+++ vue/src/views/AllHomework.vue
@@ -1,23 +1,42 @@
 <template>
   <div class="homework-list">
-      <h1>Your Upcoming Assignments: </h1>
-      <course-homework/>
+    <div class="all-homework">
+      <h1>Upcoming Assignments</h1>
+      <course-homework />
+    </div>
+      <div id="all-homework-page-sidebar">
+        <user-sidebar/>
+    </div>
   </div>
 </template>
 
 <script>
 
+import UserSidebar from '../components/UserSidebar.vue';
 import CourseHomework from '../components/CourseHomework.vue'
 
 export default {
-  components: { CourseHomework },
+  components: { UserSidebar, CourseHomework },
     name: "homework-list",
     component: {
+        UserSidebar,
         CourseHomework
     }
 }
 </script>
 
 <style>
+.all-homework {
+  grid-area: allhomework;
+}
+#all-homework-page-sidebar {
+  grid-area: sidebar;
+}
 
+.homework-list {
+  display: grid;
+  grid-template-columns: 1fr 4fr;
+  grid-template-areas: 
+  "sidebar allcourses"
+}
 </style>
