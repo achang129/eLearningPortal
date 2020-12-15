9afddee90a402561d1ffc6ca2fddfaa0986c2a01
randanno tallini <slpyrndy@gmail.com>
2020-12-15 12:08:11 -0500

    asdfasdf whoops

--- java/database/clearsdatabutleavestables.sql
+++ java/database/clearsdatabutleavestables.sql
@@ -14,4 +14,12 @@ DELETE FROM  users;
 
 
 
+INSERT INTO users (username,password_hash,role) VALUES ('user','$2a$08$UkVvwpULis18S19S5pZFn.YHPZt3oaqHZnDwqbCW9pft6uFtkXKDC','ROLE_USER');
+INSERT INTO users (username,password_hash,role) VALUES ('admin','$2a$08$UkVvwpULis18S19S5pZFn.YHPZt3oaqHZnDwqbCW9pft6uFtkXKDC','ROLE_ADMIN');
+INSERT INTO users (username,password_hash,role) VALUES ('student','$2a$08$UkVvwpULis18S19S5pZFn.YHPZt3oaqHZnDwqbCW9pft6uFtkXKDC','ROLE_STUDENT');
+INSERT INTO users (username,password_hash,role) VALUES ('teacher','$2a$08$UkVvwpULis18S19S5pZFn.YHPZt3oaqHZnDwqbCW9pft6uFtkXKDC','ROLE_TEACHER');
+
+
+
+
 COMMIT TRANSACTION;
