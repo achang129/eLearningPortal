b563e8447e0aaf8ce9e52718a3d1cecb99d112fe
randanno tallini <slpyrndy@gmail.com>
2020-12-15 11:21:04 -0500

    clear table data to test DAO, will be uploading extensive test data file

---
+++ java/database/clears_data_but_leaves_tables.sql
@@ -0,0 +1,17 @@
+BEGIN TRANSACTION;
+
+DELETE FROM mcchoice;
+DELETE FROM  grade;
+DELETE FROM  message;
+DELETE FROM  question;
+DELETE FROM  curriculum;
+DELETE FROM  teacher;
+DELETE FROM  student;
+DELETE FROM  answer;
+DELETE FROM  assignment;
+DELETE FROM  course;
+DELETE FROM  users;
+
+
+
+COMMIT TRANSACTION;
