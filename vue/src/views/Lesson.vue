f0b5e85984256520ee748b55ea0c0ac35f2d2bde
randanno tallini <slpyrndy@gmail.com>
2020-12-17 15:03:40 -0500

    eventually will format this page

--- vue/src/views/Lesson.vue
+++ vue/src/views/Lesson.vue
@@ -1,12 +1,16 @@
 <template>
-  <div class="lesson">
-      <router-link
+  <div id="lesson-grid-container" class="lesson">
+      <router-link id="link-to-courses-in-lesson"
           :to="{name: 'course', params: {id: this.courseid}}">
           <span>Go Back to Course Page</span>
         </router-link>
       <h1></h1>
       <div id="lesson">
-        <p><span v-html="this.lesson"></span></p>
+      </div>
+      <div>
+        <p id="inner-lesson-view">
+          <span v-html="this.lesson"></span>
+        </p>
       </div>
   </div>
 </template>
@@ -23,7 +27,24 @@ export default {
     }
 }
 </script>
-
 <style>
-
+#lesson {
+  grid-area: outer;
+}
+#inner-lesson-view {
+  grid-area: inner;
+}
+#link-to-courses-in-lesson{
+  grid-area: linkback
+}
+#lesson-grid-container{
+  grid-template-columns: .8fr 2fr 2fr 2fr .8fr;
+  grid-template-areas: 
+  ". linkback . . ."
+  "outer inner inner inner outer"
+   "outer inner inner inner outer"
+    "outer inner inner inner outer"
+     "outer inner inner inner outer"
+      "outer inner inner inner outer"
+}
 </style>
